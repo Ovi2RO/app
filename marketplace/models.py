91c9abb152eca277b4f30619ef55b109fc0cb02c
blister-pack <andrecrujo@hotmail.com>
2023-12-10 20:17:55 +0100

    category for marketplace posts is now a list to choose

--- marketplace/models.py
+++ marketplace/models.py
@@ -12,6 +12,17 @@ class MarketplaceItemPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    # aux
+    category_choices = [
+        ("technology", "Technology"),
+        ("services", "Services"),
+        ("vehicles", "Vehicles"),
+        ("fashion_beauty", "Fashion & Beauty"),
+        ("furniture", "Furniture"),
+        ("animals", "Animals"),
+        ("property_for_rent", "Property for rent"),
+        ("books", "Books"),
+    ]
 
     # these can be created and updated
     title = models.CharField(max_length=100)
@@ -19,7 +30,10 @@ class MarketplaceItemPost(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=200, blank=True, null=True)
     category = models.CharField(
-        max_length=100, blank=True, null=True
+        max_length=100,
+        blank=True,
+        null=True,
+        choices=category_choices,
     )  # this should be a list with several options
     # image = models.ImageField()  # gotta finish this one later
 
