d2681679e1a2e8e1e67f49b6c307506ffc7a6e60
Ovidiu <ovi_2ro@yahoo.com>
2024-01-24 09:21:23 +0100

    changing the settings

--- dj_proj/settings.py
+++ dj_proj/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lh31!^!y-0i%@=js=g4+@8$&5=c0fr*nif7geja=gr5i#*wuog"
+SECRET_KEY = os.getenv("SECRET_KEY", 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.getenv("DEBUG", False) == "True"
 
 ALLOWED_HOSTS = []
 DATE_INPUT_FORMAT=['%d/%m/%Y']
@@ -99,20 +104,33 @@ WSGI_APPLICATION = "dj_proj.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "app",
-        "USER": "ovidiu",
-        "PASSWORD": "dbadmin",
-        "HOST": "206.81.28.205",
-        "PORT": "5432",
-    },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", 'postgres'),
+        'USER': os.getenv("DB_USER", 'postgres'),
+        'PASSWORD': os.getenv("DB_PASSWORD", 'postgres'),
+        'HOST': os.getenv("HOST", 'localhost'),
+        'PORT': os.getenv("PORT", '5432'),
+    }
 }
+
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "app",
+#         "USER": "ovidiu",
+#         "PASSWORD": "dbadmin",
+#         "HOST": "206.81.28.205",
+#         "PORT": "5432",
+#     },
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+#     # }
+
+# }
 
 
 # Password validation
